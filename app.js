59d160938a78ba4f4f310583da8ea4ec6240613d
Mohamed Elhadouchi <melhadouchi387@gmail.com>
2021-09-20 20:19:14 +0100

    add a msg if you click the hate me button

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ hateBtn.addEventListener("mouseover", () => {
   hateBtn.style.top = `${Math.floor(Math.random() * window.innerHeight)}px`;
   hateBtn.style.left = `${Math.floor(Math.random() * window.innerWidth)}px`;
 });
+hateBtn.addEventListener("click", () => {
+  alert("you Got Me . MotherF***er 😡😡");
+});
 
 LoveBtn.addEventListener("click", () => {
   alert("I love You To");
