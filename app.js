613f9864963987b033971988247ae45958ed5ca0
Mohamed Elhadouchi <melhadouchi387@gmail.com>
2021-09-20 22:14:03 +0100

    fix disappeared btn

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const hateBtn = document.querySelector("#hate");
 const LoveBtn = document.querySelector("#love");
 
 hateBtn.addEventListener("mouseover", () => {
-  hateBtn.style.top = `${Math.floor(Math.random() * window.innerHeight)}px`;
-  hateBtn.style.left = `${Math.floor(Math.random() * window.innerWidth)}px`;
+  hateBtn.style.top = `${randNum(window.innerHeight)}px`;
+  hateBtn.style.left = `${randNum(window.innerWidth)}px `;
 });
 hateBtn.addEventListener("click", () => {
   alert("you Got Me . MotherF***er 😡😡");
@@ -12,3 +12,10 @@ hateBtn.addEventListener("click", () => {
 LoveBtn.addEventListener("click", () => {
   alert("I love You To");
 });
+
+function randNum(num) {
+  const randNum = Math.floor(Math.random() * num);
+  if (randNum > num) {
+    return num - 100;
+  } else return randNum;
+}
